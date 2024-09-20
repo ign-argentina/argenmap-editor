e2a0052cdfc76d7a0638f8308ca94face3f45250
afcirillo96 <agustincirillo@hotmail.com>
2024-09-20 11:28:33 -0300

    react-color dependency replaced

--- src/app/components/ColorPickerControl.js
+++ src/app/components/ColorPickerControl.js
@@ -1,48 +1,30 @@
 import React, { useState } from 'react';
 import { withJsonFormsControlProps } from '@jsonforms/react';
-import { SketchPicker } from 'react-color'; 
 
 const MaterialInputControl = ({ handleChange, data, path }) => {
-  const [displayColorPicker, setDisplayColorPicker] = useState(false);
   const [currentColor, setCurrentColor] = useState(data || '#ffffff');
 
-  const handleButtonClick = () => {
-    setDisplayColorPicker(!displayColorPicker);
-    console.log(data, path)
-  };
-
-  const handleColorChange = (color) => {
-    setCurrentColor(color.hex);
-    handleChange(path, color.hex);
+  const handleColorChange = (event) => {
+    const newColor = event.target.value;
+    setCurrentColor(newColor);
+    handleChange(path, newColor);
   };
 
   return (
     <div>
       <label>{path}</label>
-      <button
+      <input
+        type="color"
+        value={currentColor}
+        onChange={handleColorChange}
         style={{
-          backgroundColor: currentColor,
           width: '100%',
-          height: '25px',
+          height: '40px',
           border: 'none',
           cursor: 'pointer',
           borderRadius: '4px',
         }}
-        onClick={handleButtonClick}
       />
-      
-      {displayColorPicker && (
-        <div style={{ position: 'absolute', zIndex: 2 }}>
-          <div
-            style={{ position: 'fixed', top: 0, right: 0, bottom: 0, left: 0 }}
-            onClick={() => setDisplayColorPicker(false)}
-          />
-          <SketchPicker
-            color={currentColor}
-            onChangeComplete={handleColorChange}
-          />
-        </div>
-      )}
     </div>
   );
 };
