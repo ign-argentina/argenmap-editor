1fba6dab3d12eb33aafa51f91f42b67a815c62a5
afcirillo96 <agustincirillo@hotmail.com>
2024-09-20 16:04:15 -0300

    branch master added

--- src/app/page.js
+++ src/app/page.js
@@ -9,113 +9,6 @@ import ColorPickerControl from '../app/components/ColorPickerControl';
 import { rankWith, schemaMatches, uiTypeIs, and } from '@jsonforms/core';
 
 
-// const NEWschema = {
-//   "type": "object",
-//   "properties": {
-//     "app": {
-//       "type": "object",
-//       "properties": {
-//         "version": { "type": "string" }
-//       }
-//     },
-//     "ui": {
-//       "type": "object",
-//       "properties": {
-//         "theme": {
-//           "type": "object",
-//           "properties": {
-//             "bodyBackground": { "type": "string", "format": "color" },
-//             "headerBackground": { "type": "string", "format": "color" },
-//             "menuBackground": { "type": "string", "format": "color" },
-//             "activeLayer": { "type": "string", "format": "color" },
-//             "textMenu": { "type": "string" },
-//             "textMenuStyle": { "type": "string" },
-//             "textLegendMenu": { "type": "string", "format": "color" },
-//             "textLegendMenuStyle": { "type": "string" },
-//             "iconBar": { "type": "string", "format": "color" }
-//           }
-//         }
-//       }
-//     },
-//     "resources": {
-//       "type": "object",
-//       "properties": {
-//         "leaflet": { "type": "string" }
-//       }
-//     }
-//   }
-// }
-
-// const NEWuischema =  {
-//   "type": "VerticalLayout",
-//   "elements": [
-//     {
-//       "type": "Group",
-//       "label": "App Configuration",
-//       "elements": [
-//         {
-//           "type": "Control",
-//           "scope": "#/properties/app/properties/version",
-//           "label": "App Version"
-//         }
-//       ]
-//     },
-//     {
-//       "type": "Group",
-//       "label": "UI Theme",
-//       "elements": [
-//         {
-//           "type": "Control",
-//           "scope": "#/properties/ui/properties/theme/properties/bodyBackground",
-//           "label": "Body Background Color"
-//         },
-//         {
-//           "type": "Control",
-//           "scope": "#/properties/ui/properties/theme/properties/headerBackground",
-//           "label": "Header Background Color"
-//         },
-//         {
-//           "type": "Control",
-//           "scope": "#/properties/ui/properties/theme/properties/menuBackground",
-//           "label": "Menu Background Color"
-//         },
-//         {
-//           "type": "Control",
-//           "scope": "#/properties/ui/properties/theme/properties/activeLayer",
-//           "label": "Active Layer Color"
-//         },
-//         {
-//           "type": "Control",
-//           "scope": "#/properties/ui/properties/theme/properties/textMenu",
-//           "label": "Text Menu Color"
-//         },
-//         {
-//           "type": "Control",
-//           "scope": "#/properties/ui/properties/theme/properties/textLegendMenu",
-//           "label": "Text Legend Menu Color"
-//         },
-//         {
-//           "type": "Control",
-//           "scope": "#/properties/ui/properties/theme/properties/iconBar",
-//           "label": "Icon Bar Color"
-//         }
-//       ]
-//     },
-//     {
-//       "type": "Group",
-//       "label": "Resources",
-//       "elements": [
-//         {
-//           "type": "Control",
-//           "scope": "#/properties/resources/properties/leaflet",
-//           "label": "Leaflet URL"
-//         }
-//       ]
-//     }
-//   ]
-// }
-
-
 export default function Page() {
   const { config, loading: configLoading, error: configError } = useConfig();
   const [data, setData] = useState({});
@@ -267,7 +160,7 @@ export default function Page() {
           <img src="/logos/logo.png" alt="Logo" className="logo" />
         </div>
         <div className="version-info">
-          <label>Versión de Config: {config ? config.app.version : 'Sin versión...'}</label>
+          <label>v{config ? config.app.version : 'Sin versión...'}</label>
         </div>
 
         <button className="showHide-button" onClick={() => setIsFormShown(!isFormShown)} title="Mostrar/Ocultar Formularios">
