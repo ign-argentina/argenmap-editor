55c77dd4ecaa72a29cf9bd0bf8f6c43b07e4f09d
afcirillo96 <agustincirillo@hotmail.com>
2024-09-27 17:22:30 -0300

    menu refactor

--- src/app/page.js
+++ src/app/page.js
@@ -192,27 +192,32 @@ export default function Page() {
     <div className="editor-container">
       <div className='navbar'>
         <div className="logo-container">
-          <img src="/logos/logo.png" alt="Logo" className="logo" />
+          <img src="/logos/logo2.png" alt="Logo" className="logo" />
         </div>
         <div className="version-info">
-          <label>v{config ? config.app.version : 'Sin versión...'}</label>
+          <label>EDITOR v{config ? config.app.version : 'Sin versión...'}</label>
         </div>
 
-        <button className="showHide-button" onClick={() => setIsFormShown(!isFormShown)} title="Mostrar/Ocultar Formularios">
-          <i className={isFormShown ? "fa-solid fa-eye" : "fa-solid fa-eye-slash"}></i>
-        </button>
+        <div className="button-container">
+          <div className="select-container">
+            <i className="fa-solid fa-earth-americas"></i>
+            <select className="lang-select" onChange={handleLanguageChange} value={selectedLang}>
+              {language && Object.keys(language).map((langKey) => (
+                <option key={langKey} value={langKey}>
+                  {langKey === 'default' ? 'Predeterminado' : langKey.charAt(0).toUpperCase() + langKey.slice(1)}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <div className="select-container">
-          <i className="fa-solid fa-earth-americas"></i>
-          <select className="lang-select" onChange={handleLanguageChange} value={selectedLang}>
-            {language && Object.keys(language).map((langKey) => (
-              <option key={langKey} value={langKey}>
-                {langKey === 'default' ? 'Predeterminado' : langKey.charAt(0).toUpperCase() + langKey.slice(1)}
-              </option>
-            ))}
-          </select>
+          <button className="clear-storage" onClick={handleClearStorage} title="Limpiar Memoria">
+            <i className="fa-solid fa-trash-can"></i>
+          </button>
+
+          <button className="showHide-button" onClick={() => setIsFormShown(!isFormShown)} title="Mostrar/Ocultar Formularios">
+            <i className={isFormShown ? "fa-solid fa-play" : "fa-solid fa-play fa-flip-horizontal"}></i>
+          </button>
         </div>
-
 
         {sectionKeys.map((key) => (
           <button
@@ -234,12 +239,11 @@ export default function Page() {
           </button>
         ))}
 
-        <button className="clear-storage" onClick={handleClearStorage} title="Limpiar Memoria">
-          <i className="fa-solid fa-trash-can"></i>
-        </button>
-
         <button className="download-button" onClick={handleDownload} title="Descargar JSON">
-          <i className="fa-solid fa-download"></i>
+          <span className="icon">
+            <i className="fa-solid fa-download"></i>
+          </span>
+          Descargar
         </button>
       </div>
 
