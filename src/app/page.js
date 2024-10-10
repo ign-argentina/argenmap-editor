4cfe6f34f3c04823c0ea798571730b5de8f41704
afcirillo96 <agustincirillo@hotmail.com>
2024-10-10 15:54:46 -0300

    small update

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ import { materialCells, materialRenderers } from '@jsonforms/material-renderers'
 import { rankWith, schemaMatches, uiTypeIs, and } from '@jsonforms/core';
 import Preview from './components/Preview';
 import Navbar from './components/Navbar';
-import Welcome from './components/Welcome';
 import ColorPickerControl from './utils/ColorPickerControl';
 import TranslateSchema from './utils/TranslateSchema';
 import GenerateSchema from './utils/GenerateSchema';
@@ -18,12 +17,6 @@ import Ajv from 'ajv';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export default function Page() {
-  // const { schema, data, config, language, selectedLang } = useData();
-  // console.log("schema: ", schema)
-  // console.log("data: ", data)
-  // console.log("config: ", config)
-  // console.log("selectedLang: ", selectedLang)
-
   const { config, loading: configLoading, error: configError } = useConfig();
   const { lang: language, loading: langLoading, error: langError } = useLang();
   const [data, setData] = useState({});
@@ -31,19 +24,15 @@ export default function Page() {
   const [schema, setSchema] = useState({});
   const [isFormShown, setIsFormShown] = useState(true);
   const [toast, setToast] = useState(null);
-
-  // Loads lang from localStorage, if it doesnt exists, use a default
   const savedLanguage = localStorage.getItem('selectedLang') || 'es';
-  const [selectedLang, setSelectedLang] = useState(savedLanguage);
+  const [selectedLang, setSelectedLang] = useState(savedLanguage);  // Loads lang from localStorage, if it doesnt exists, use a default
 
   const ajv = new Ajv();
   ajv.addFormat('color', /^#[0-9A-Fa-f]{6}$/); // Custom format "color" added to JSONForms
-
   const showToast = (message, type) => { // Toast (message)
     setToast({ message, type });
     setTimeout(() => setToast(null), 3000);
   };
-
   const colorPickerTester = rankWith(
     3,
     and(uiTypeIs('Control'), schemaMatches((schema) => schema.format === 'color'))
@@ -72,8 +61,8 @@ export default function Page() {
       } else {
         setSelectedSection(null);
       }
+      return translatedSchema
     }
-    
   }
 
   // Load saved data from localStorage on startup.
@@ -83,13 +72,9 @@ export default function Page() {
       const parsedStoredData = JSON.parse(storedData);
       setData(parsedStoredData);
       uploadData();
-      console.log("setData localStorage")
-
     } else if (config) {
       setData(config);
       uploadData();
-      console.log("setData config")
-
     }
   }, [config, language, selectedLang]);
 
@@ -97,10 +82,10 @@ export default function Page() {
 
   const handleJsonUpload = (parsedData) => {
     setData(parsedData);
-    uploadData();
+    const uploadedSchema = uploadData();
+    setSchema(uploadedSchema)
     window.location.reload();
     showToast('JSON cargado exitosamente', 'success');
-    console.log("setData upload")
   };
 
 
@@ -132,7 +117,7 @@ export default function Page() {
     localStorage.setItem("formData", JSON.stringify(formData));
 
     setData(config);
-    window.location.reload();
+    // window.location.reload();
     showToast("¡Los valores del formData se han limpiado con éxito!", "success");
   };
 
@@ -159,7 +144,6 @@ export default function Page() {
           handleJsonUpload={handleJsonUpload}
           setIsFormShown={setIsFormShown}
         />
-        {/* <Welcome onJsonUpload={handleJsonUpload}/> */}
 
         {isFormShown && (
           <div className="form-container">
@@ -203,175 +187,3 @@ export default function Page() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-// 'use client'
-// import { useEffect, useState } from 'react';
-// import { JsonForms } from '@jsonforms/react';
-// import { materialCells, materialRenderers } from '@jsonforms/material-renderers';
-// import { rankWith, schemaMatches, uiTypeIs, and } from '@jsonforms/core';
-// import Preview from './components/Preview';
-// import Navbar from './components/Navbar';
-// import ColorPickerControl from './utils/ColorPickerControl';
-// import HandleDownload from './utils/HandleDownload';
-// import Toast from './utils/Toast';
-// import Ajv from 'ajv';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import useData from '../app/hooks/useData';
-
-// export default function Page() {
-//   const { schema, dataHook, config, language, selectedLang, setSelectedLang } = useData();
-//   console.log("schema: ", schema)
-//   console.log("dataHook: ", dataHook)
-//   console.log("config: ", config)
-
-
-//   const [data, setData] = useState({});
-//   const [selectedSection, setSelectedSection] = useState(null);
-//   const [isFormShown, setIsFormShown] = useState(true);
-//   const [toast, setToast] = useState(null);
-
-//   // Loads lang from localStorage, if it doesnt exists, use a default
-//   const savedLanguage = localStorage.getItem('selectedLang') || 'es';
-//   // const [selectedLang, setSelectedLang] = useState(savedLanguage);
-
-//   const ajv = new Ajv();
-//   ajv.addFormat('color', /^#[0-9A-Fa-f]{6}$/); // Custom format "color" added to JSONForms
-
-//   const showToast = (message, type) => { // Toast (message)
-//     setToast({ message, type });
-//     setTimeout(() => setToast(null), 3000);
-//   };
-
-//   const colorPickerTester = rankWith(
-//     3,
-//     and(uiTypeIs('Control'), schemaMatches((schema) => schema.format === 'color'))
-//   );
-//   const customRenderers = [
-//     ...materialRenderers,
-//     { tester: colorPickerTester, renderer: ColorPickerControl }
-//   ];
-//   const sectionKeys = schema && schema.properties ? Object.keys(schema.properties) : [];
-
-
-//   // Load saved data from localStorage on startup.
-//   useEffect(() => {
-//     setData(dataHook)
-
-//   }, [config, language, selectedLang]);
-
-  
-
-//   const handleJsonUpload = (parsedData) => {
-//     setData(parsedData);
-//     uploadData();
-//     window.location.reload();
-//     showToast('JSON cargado exitosamente', 'success');
-//     console.log("setData upload")
-//   };
-
-
-
-//   const handleSectionChange = (section) => {
-//     setSelectedSection(section);
-//   };
-
-//   const handleLanguageChange = (e) => {
-//     const selectedLanguage = e.target.value;
-//     setSelectedLang(selectedLanguage);
-//     localStorage.setItem('selectedLang', selectedLanguage);
-//   };
-
-//   const handleClearStorage = () => {
-//     const formData = JSON.parse(localStorage.getItem("formData"));
-
-//     const clearValues = (data) => {
-//       for (const key in data) {
-//         if (typeof data[key] === "object" && data[key] !== null) {
-//           clearValues(data[key]);
-//         } else {
-//           data[key] = "";
-//         }
-//       }
-//     };
-//     clearValues(formData);
-//     localStorage.setItem("formData", JSON.stringify(formData));
-
-//     setData(config);
-//     window.location.reload();
-//     showToast("¡Los valores del formData se han limpiado con éxito!", "success");
-//   };
-
-
-//   const { downloadJson } = HandleDownload({ data, config });
-//   const handleDownload = () => {
-//     downloadJson();
-//   };
-
-//   return (
-//     <div>
-//       <div className="editor-container">
-//         <Navbar
-//           config={config}
-//           language={language}
-//           selectedLang={'en'}
-//           handleLanguageChange={handleLanguageChange}
-//           handleClearStorage={handleClearStorage}
-//           sectionKeys={sectionKeys}
-//           selectedSection={selectedSection}
-//           handleSectionChange={handleSectionChange}
-//           isFormShown={isFormShown}
-//           handleDownload={handleDownload}
-//           handleJsonUpload={handleJsonUpload}
-//           setIsFormShown={setIsFormShown}
-//         />
-//         {/* <Welcome onJsonUpload={handleJsonUpload}/> */}
-
-//         {isFormShown && (
-//           <div className="form-container">
-//             {selectedSection && (
-//               <div className="custom-form-group">
-//                 <JsonForms
-//                   schema={schema.properties[selectedSection]}
-//                   data={data[selectedSection]}
-//                   renderers={customRenderers}
-//                   cells={materialCells}
-//                   ajv={ajv}
-//                   onChange={({ data: updatedData }) => {
-//                     setData((prevData) => {
-//                       const newData = {
-//                         ...prevData,
-//                         [selectedSection]: updatedData
-//                       };
-//                       localStorage.setItem('formData', JSON.stringify(newData));
-//                       return newData;
-//                     });
-//                   }}
-//                 />
-//               </div>
-//             )}
-//           </div>
-//         )}
-
-//         {toast && (
-//           <Toast
-//             message={toast.message}
-//             type={toast.type}
-//             duration={3000}
-//             onClose={() => setToast(null)}
-//           />
-//         )}
-
-//         <div className="preview-container">
-//           <Preview />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
