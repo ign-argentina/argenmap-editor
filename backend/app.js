e602e09fe818d0b32acb7efdaca8c9c5e08fb1bf
Franco Leonel Cristillo <104926556+FRLCR@users.noreply.github.com>
2025-05-13 14:50:12 -0300

    Cors config
    Allow only origin fetchs.

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser"
 import cors from "cors";
 import dotenv from 'dotenv'
 import routes from './routes/routes.js'
@@ -6,19 +7,23 @@ import routes from './routes/routes.js'
 dotenv.config()
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-console.log(process.env.PORT)
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
+app.use(cookieParser());
 
 app.get("/", (_req, res) => {
   res.json("API Editor Argenmap" );
 });
 
-/****** USO DE RUTAS  ******/
-//app.use(`/v1/editor`, routes);
+
+// Uso de rutas
 app.use(`/`, routes);
 
 app.listen(PORT, () => {
