55957223b77db0dcfb367163a17dc7ad17107d0d
afcirillo96 <agustincirillo@hotmail.com>
2025-05-21 14:51:42 -0300

    welcomePage added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,7 +57,7 @@ function Navbar() {
                     <img src="https://static.ign.gob.ar/img/logo/ign/logo_IGN_blanco_sinTexto.svg" alt="Logo IGN" />
                 </div>
                 <div className="nav-links">
-                    <NavLink to="/" className={({ isActive }) => (isActive ? "active" : undefined)}>Home</NavLink>
+                    <NavLink to="/welcome-page" className={({ isActive }) => (isActive ? "active" : undefined)}>Home</NavLink>
                     <NavLink to="/ejemplo" className={({ isActive }) => (isActive ? "active" : undefined)}>Navlink de Ejemplo</NavLink>
                     <NavLink to="/ejemploDos" className={({ isActive }) => (isActive ? "active" : undefined)}>
                         <i className="fa-solid fa-right-from-bracket"></i> Navlink de Ejemplo
@@ -69,7 +69,7 @@ function Navbar() {
                                 <i className="fa-solid fa-right-from-bracket"></i>Iniciar Sesión
                             </button>
                             <button className="nav-button" onClick={() => setShowRegisterModal(true)} title="Registrarse">
-                                <i class="fa-solid fa-user-plus"></i> Registrarse
+                                <i className="fa-solid fa-user-plus"></i> Registrarse
                         </button>
                 </>
                 ) : (
--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,14 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Root";
+import WelcomePage from '../components/WelcomePage';
 
 const router = createBrowserRouter([
-    {
-        path: "/", element: <Root />, children: [
-            { path: "/", element: <><h1>ACA VA UNA VISTA!</h1></> },
-            { path: "/ejemplo", element: <><h1>PUERTO RIO PLATA CALAMAR!</h1></> },
-            { path: "/ejemploDos", element: <><h1>ACCEDISTE A LA RUTA EJEMPLO DOS!</h1></> },
-        ]
-    },
+  {
+    path: "/", element: <Root />, children: [
+      { path: "/", element: <><h1>ACA VA UNA VISTA!</h1></> },
+      { path: "/ejemplo", element: <><h1>PUERTO RIO PLATA CALAMAR!</h1></> },
+      { path: "/ejemploDos", element: <><h1>ACCEDISTE A LA RUTA EJEMPLO DOS!</h1></> },
+      { path: "/welcome-page", element: <WelcomePage /> },
+
+    ]
+  },
 ])
 
 export default router
