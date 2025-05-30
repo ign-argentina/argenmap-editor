c4475f3c26bd048557a53210e664b226df851d50
afcirillo96 <agustincirillo@hotmail.com>
2025-05-30 15:56:23 -0300

    css fix

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -3,20 +3,17 @@ import './WelcomePage.css';
 
 const WelcomePage = () => {
 
-
   return (
-    <div >
-      <div className="welcome-screen">
-          <div className="welcome-screen-modal">
-            <h2>BIENVENIDO AL EDITOR ARGENMAP</h2>
-            <p>
-              Un editor de archivos JSON fácil de usar para facilitar la creación, edición y validación de la configuración del visor Argenmap.
-            </p>
-            <p style={{ marginTop: '20px' }}>Para ver los visores, entre aquí:</p>
-            <button className="visor-manager-button" onClick={() => setIsVisorManagerVisible(true)}>
-              <i className="fa-solid fa-eye"></i> Visor Manager
-            </button>
-          </div>
+    <div className="welcome-screen">
+      <div className="welcome-screen-modal">
+        <h2>BIENVENIDO AL EDITOR ARGENMAP</h2>
+        <p>
+          Un editor de archivos JSON fácil de usar para facilitar la creación, edición y validación de la configuración del visor Argenmap.
+        </p>
+        <p style={{ marginTop: '20px' }}>Para ver los visores, entre aquí:</p>
+        <button className="visor-manager-button" onClick={() => setIsVisorManagerVisible(true)}>
+          <i className="fa-solid fa-eye"></i> Visor Manager
+        </button>
       </div>
     </div>
   );
