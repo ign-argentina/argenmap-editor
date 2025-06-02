af894cffaddcfeae8a8617148c1f38bda48ab269
Franco Leonel Cristillo <104926556+FRLCR@users.noreply.github.com>
2025-06-02 13:55:15 -0300

    Update groupRoutes.js

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -11,7 +11,7 @@ groupRoutes.delete("/", PROTECT.REQUIRE_GROUP_ADMIN, groupController.deleteGroup
 groupRoutes.get("/", PROTECT.REQUIRE_AUTH, groupController.getGroupList) // Devuelve el listado de id's grupos al cual uno pertenece. Si sos superadmin, todos. Si no, tus grupos
 
 groupRoutes.post("/management", PROTECT.REQUIRE_GROUP_ADMIN, groupController.addUserToGroup)
-groupRoutes.delete("/management", groupController.deleteUserFromGroup)
+groupRoutes.delete("/management", PROTECT.REQUIRE_GROUP_ADMIN, groupController.deleteUserFromGroup)
 groupRoutes.get("/management", PROTECT.REQUIRE_GROUP_ADMIN, groupController.getManageGroupList) // Si sos superadmin, devuelve todos los grupos. Si no, los grupos que administras
 groupRoutes.get("/management/:id", PROTECT.REQUIRE_GROUP_ADMIN, groupController.getGroup) // Devuelve informacion del grupo si sos admin del grupo o superadmin
 groupRoutes.get("/management/userlist/:id", PROTECT.REQUIRE_GROUP_ADMIN, groupController.getGroupUserList)
