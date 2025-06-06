88da46d5a626d8bad9422e38f8e4793cf376b3c2
Franco Leonel Cristillo <104926556+FRLCR@users.noreply.github.com>
2025-06-06 09:00:25 -0300

    Update visoresRoutes.js

--- backend/routes/visoresRoutes.js
+++ backend/routes/visoresRoutes.js
@@ -6,7 +6,7 @@ const visorController = new VisorController();
 
 // placesRoutes.get("/:id", placesController.getPlaceById); // Ideal por parámetro y no por query al ser búsqueda simple.
 
-visoresRoutes.post("/", PROTECT.REQUIRE_GROUP_ADMIN, visorController.createVisor);
+visoresRoutes.post("/", PROTECT.REQUIRE_AUTH, visorController.createVisor);
 visoresRoutes.put("/updateVisor", visorController.updateVisor)
 visoresRoutes.get("/", visorController.getAllVisors);
 visoresRoutes.get("/:id", visorController.getVisorById);
