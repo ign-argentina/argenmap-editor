f35c495cd856b04457c4034003f56440a2b1a104
Franco Leonel Cristillo <104926556+FRLCR@users.noreply.github.com>
2025-06-09 09:21:46 -0300

    Update routes.js
    JSDoc

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,6 +8,18 @@ import rolesRoutes from "./rolesRoutes.js";
 
 const routes = Router();
 
+/**
+ * Rutas principales de la API.
+ * 
+ * Rutea los endpoints específicos para cada recurso:
+ * - /visores  => rutas relacionadas con visores
+ * - /configs  => rutas de configuración
+ * - /users    => rutas para manejo de usuarios
+ * - /auth     => rutas de autenticación
+ * - /groups   => rutas de grupos
+ * - /roles    => rutas de roles
+ */
+
 routes.use("/visores", visoresRoutes);
 routes.use("/configs", configRoutes);
 routes.use("/users", userRoutes)
