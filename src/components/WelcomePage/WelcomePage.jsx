636e862c2b9f9c608551d0dd4aea6e62f0413b0c
afcirillo96 <agustincirillo@hotmail.com>
2025-06-27 16:16:37 -0300

    more readme updates

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -17,7 +17,7 @@ const WelcomePage = () => {
         <div className="welcome-screen-modal">
           <h2>BIENVENIDO AL EDITOR ARGENMAP</h2>
           <p>
-            Un editor de archivos JSON fácil de usar para facilitar la creación, edición y validación de la configuración del visor Argenmap.
+            Es una aplicación web desarrollada para crear y gestionar visores hechos con Argenmap.
           </p>
           <p style={{ marginTop: '20px' }}>Para ver los visores, entre aquí:</p>
           <button className="welcome-page-button" onClick={() => navigate('/visores')}>
