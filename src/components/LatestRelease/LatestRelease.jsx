e1007bd0acdac0b1e5a3f061a5236bb0b446249f
afcirillo96 <agustincirillo@hotmail.com>
2025-07-28 13:05:49 -0300

    footer css glow up

--- src/components/LatestRelease/LatestRelease.jsx
+++ src/components/LatestRelease/LatestRelease.jsx
@@ -1,4 +1,5 @@
 import { useLatestRelease } from "/src/hooks/useLatestRelease";
+import '../Footer/Footer.css'
 
 function LatestRelease() {
   const { release, error } = useLatestRelease();
@@ -6,7 +7,7 @@ function LatestRelease() {
   if (error) return <div>{error}</div>;
   if (!release) return <div>Cargando...</div>;
 
-  return <div><h1>{release.tag_name}</h1></div>;
+  return <div><h1 className="release-version">{release.tag_name}</h1></div>;
 }
 
 export default LatestRelease;
