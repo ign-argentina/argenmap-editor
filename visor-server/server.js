753d498399babd1cef1e97f2dc79249e860d72fe
Franco Leonel Cristillo <104926556+FRLCR@users.noreply.github.com>
2025-08-28 11:42:25 -0300

    Argenmap update

--- visor-server/server.js
+++ visor-server/server.js
@@ -69,6 +69,7 @@ app.post('/argenmap/custom', (req, res) => {
         window.appPreferences = ${JSON.stringify(preferencesJson)};
       </script>
     `;
+
     html = html.replace('</head>', `${injectScript}\n</head>`);
 
     res.status(200).send(html);
