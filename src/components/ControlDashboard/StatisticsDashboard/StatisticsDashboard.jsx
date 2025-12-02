0198db24d2f0051dfc75f195b4b74a4a404a9ff3
afcirillo96 <agustincirillo@hotmail.com>
2025-12-02 12:19:57 -0300

    Linechart & Barchart html added

--- src/components/ControlDashboard/StatisticsDashboard/StatisticsDashboard.jsx
+++ src/components/ControlDashboard/StatisticsDashboard/StatisticsDashboard.jsx
@@ -6,6 +6,14 @@ import {
   ResponsiveContainer,
   Tooltip,
   Legend,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  BarChart,
+  Bar,
+  Rectangle,
 } from "recharts";
 
 import {
@@ -111,6 +119,47 @@ function StatisticsDashboard() {
   return (
     <div className="statistics-dashboard">
       <section className="sd-body">
+
+        <LineChart
+          style={{ width: '100%', maxWidth: '700px', height: '100%', maxHeight: '70vh', aspectRatio: 1.618 }}
+          responsive
+          data={groupPieData}
+          margin={{
+            top: 5,
+            right: 0,
+            left: 0,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis width="auto" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+
+        <BarChart
+          style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
+          responsive
+          data={groupPieData}
+          margin={{
+            top: 5,
+            right: 0,
+            left: 0,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis width="auto" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+          <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+        </BarChart>
+
         {/* ---------------- USERS ---------------- */}
         <div className="sd-column">
           <MetricCard
